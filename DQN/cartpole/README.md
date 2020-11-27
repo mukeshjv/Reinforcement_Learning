a30004c913f87727c8fe0523e4729a149ed90a42
mukesh <mukesh.fdmje17@sinhgad.edu>
2020-11-27 17:13:40 +0530

    Updated blobs and README

--- DQN/cartpole/README.md
+++ DQN/cartpole/README.md
@@ -1,29 +1,49 @@
 # DQN for cartpole-v0 using pytorch
 
-- PyTorch implementation of [DQN](https://arxiv.org/abs/1312.5602) for [cartpole-v0](https://gym.openai.com/envs/CartPole-v1/)
+- PyTorch implementation of [Double Q-learning](https://arxiv.org/pdf/1509.06461.pdf) for [CartPole-v0](https://gym.openai.com/envs/CartPole-v0/)
 
-- The neural network consists of an input layer, 3 Linear layers with RELU activations and an output layer which outputs Q values for each action for given state.
+- The neural network consists of an input layer, 2 Linear layers with RELU activations and an output layer which outputs Q values for all actions for given state.
+
 ## Dependencies
 
 ### Trained on
+
 - Python 3.7
 - PyTorch 1.5.1
 - numpy 1.18.1
 - gym 0.17.2
+  
+### Hyper Parameters
+
+- batch_size = 128
+- gamma = 0.999
+- eps_start = 0.99
+- eps_end = 0.01
+- eps_decay = 0.001
+- target_update = 5
+- memory_size = 100000
+- lr = 0.001
+
+## Usage
+
+### Play
+
+    python cartpole.py play
+
+### Train
+
+    python cartpole.py train
 
 ## Results
 
-Initial     |   After 450 episodes
+Initial     |   After 500 episodes
 :-------------------------:|:-------------------------:
-![](https://github.com/mukeshjv/Reinforcement_Learning/blob/main/DQN/cartpole/blob/start.gif)  | ![](https://github.com/mukeshjv/Reinforcement_Learning/blob/main/DQN/cartpole/blob/end.gif)
-
+![start_gif](blob/start.gif)  | ![end_gif](blob/end.gif)
 
 ## Plot
 
-- The blue line represents duration of current episode and the orange line represents average of the last 100 episodes.
+- The blue line represents the duration of current episode and the orange line represents the average of the last 100 episodes.
 
-![](https://github.com/mukeshjv/Reinforcement_Learning/blob/main/DQN/cartpole/blob/cartpole.png)
+![plot](blob/cartpole.png)
 
-
-*Yet to solve
-(CartPole-v0 defines "solving" as getting average reward of 195.0 over 100 consecutive trials).
+*The environment was solved at around 200 episodes.
--- DQN/cartpole/cartpole.py
+++ DQN/cartpole/cartpole.py
@@ -12,53 +12,70 @@ import torch.nn as nn
 import torch.optim as optim
 import torch.nn.functional as F
 import torchvision.transforms as T
+import sys
+
+device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
 
 #Container to store timestep info
 Experience = namedtuple(
     'Experience',
-    ('state', 'action', 'next_state', 'reward')
+    ('state', 'action', 'next_state', 'reward', 'done')
 )
 
+#Environement
+env = gym.make('CartPole-v0')
+num_states = env.observation_space.shape[0]
+num_actions = env.action_space.n
+
+
 #Hyper-params
-batch_size = 512
-gamma = 0.999
-target_update = 10
-memory_size = 900000
-lr = 0.001
-num_episodes = 500
-epsilon = 0.10
+batch_size = 128        #size of batch to train the network
+gamma = 0.999           #discount factor
+eps_start = 0.99        #epsilon start value
+eps_end = 0.01          #epsilon end value
+eps_decay = 0.001       #epsilon decay rate
+target_update = 5       #number of episodes to update the target network from the policy network
+memory_size = 100000    #Experience replay buffer size
+lr = 0.001              #learning rate for Adam
+num_episodes = 500      #number of episodes
 
 #Deep Q Network
 class DQN(nn.Module):
-    def __init__(self, img_height, img_width):
-        super().__init__()
+    def __init__(self, num_inputs, num_actions):
+        super(DQN, self).__init__()
+        self.fc1 = nn.Linear(in_features=num_inputs, out_features=32)       #Dense Layer 1
+        self.fc2 = nn.Linear(in_features=32, out_features=32)               #Dense Layer 2
+        self.out = nn.Linear(in_features=32, out_features=num_actions)      #Output Layer
 
-        self.fc1 = nn.Linear(in_features=img_height*img_width*3, out_features=128)   
-        self.fc2 = nn.Linear(in_features=128, out_features=64)
-        self.fc3 = nn.Linear(in_features=64, out_features=32)
-        self.out = nn.Linear(in_features=32, out_features=2)
     def forward(self, t):
-        t = t.flatten(start_dim=1)
         t = F.relu(self.fc1(t))
         t = F.relu(self.fc2(t))
-        t = F.relu(self.fc3(t))
         t = self.out(t)
         return t
 
+policy_net = DQN(num_states, num_actions).to(device)            #Network to get values for current states
+target_net = DQN(num_states, num_actions).to(device)            #Network to get values for future states
+optimizer = optim.Adam(params=policy_net.parameters(), lr=lr)   #Adam optimizer for neural network
+
 
 class Agent():
-    def __init__(self, num_actions, epsilon, device):
+    def __init__(self, num_actions, start, end, decay):
         self.current_step = 0
         self.num_actions = num_actions
-        self.epsilon = epsilon
-        self.device = device
+        self.start = start
+        self.end = end
+        self.decay = decay
 
-    def select_action(self, state, policy_net):
-        if self.epsilon > random.random():
-            action = random.randrange(self.num_actions)
-            return torch.tensor([action]).to(self.device)
+    def select_action(self, state):
+        random_number = random.random()
+        epsilon = self.end + (self.start - self.end) * \
+            math.exp(-1. * self.current_step * self.decay)
+        self.current_step += 1
+        if random_number < epsilon:
+            return env.action_space.sample()                    #Select random action
         else:
-            return policy_net(state).argmax(dim=1).to(self.device)
+            q_values = policy_net(state).cpu().data.numpy()     
+            return np.argmax(q_values)                          #Select the action with max Q value for the given state
 
 
 class ReplayMemory():
@@ -67,118 +84,42 @@ class ReplayMemory():
         self.memory = []
         self.mem_count = 0
 
-    def push(self, experience):     #Add experience to replay buffer
+    def push(self, experience):                                 #Add experience to the buffer
         if len(self.memory) < self.capacity:
             self.memory.append(experience)
         else:
             self.memory[self.mem_count % self.capacity] = experience
         self.mem_count += 1
 
-    def sample(self, batch_size):   #Sample a batch from the replay buffer
-        return random.sample(self.memory, batch_size)
+    def sample(self, batch_size):                               #Returns a random sample of size = batch_size from the buffer to train the network
+        states, actions, rewards, next_states, dones = [], [], [], [], []
+        idx = np.random.choice(len(self.memory), batch_size)
+        for i in idx:
+            experience = self.memory[i]
+            state, action, next_state, reward, done = experience
+            states.append(np.array(state, copy=False))
+            actions.append(np.array(action, copy=False))
+            rewards.append(reward)
+            next_states.append(np.array(next_state, copy=False))
+            dones.append(done)
+        states = torch.as_tensor(
+            np.array(states), dtype=torch.float32, device=device)
+        actions = torch.as_tensor(np.array(actions), device=device)
+        rewards = torch.as_tensor(
+            np.array(rewards, dtype=np.float32), device=device
+        )
+        next_states = torch.as_tensor(
+            np.array(next_states), dtype=torch.float32, device=device)
+        dones = torch.as_tensor(
+            np.array(dones, dtype=np.float32), device=device)
+        return states, actions, rewards, next_states, dones        
 
     def can_provide_sample(self, batch_size):
         return len(self.memory) >= batch_size
-
-#Environement
-class Env():
-    def __init__(self, device):
-        self.device = device
-        self.env = gym.make('CartPole-v0').unwrapped
-        self.env.reset()
-        self.current_screen = None
-        self.done = False
-
-    def reset(self):
-        self.env.reset()
-        self.current_screen = None
-
-    def close(self):
-        self.env.close()
-
-    def render(self, mode='human'):
-        return self.env.render(mode)
-
-    def num_actions_available(self):
-        return self.env.action_space.n
-
-    def take_action(self, action):
-        observation, reward, self.done, info = self.env.step(action.item())
-        return torch.tensor([reward], device=self.device)
-
-    def is_starting(self):
-        return self.current_screen is None
-
-    def get_state(self):
-        if self.is_starting() or self.done:
-            self.current_screen = self.get_processed_screen()
-            black_screen = torch.zeros_like(self.current_screen)
-            return black_screen
-        else:
-            screen1 = self.current_screen
-            screen2 = self.get_processed_screen()
-            self.current_screen = screen2
-            return screen2 - screen1
-
-    def get_screen_height(self):
-        return self.get_processed_screen().shape[2]
-
-    def get_screen_width(self):
-        return self.get_processed_screen().shape[3]
-
-    def get_processed_screen(self):
-        screen = self.render('rgb_array').transpose((2, 0, 1))
-        screen = self.crop_screen(screen)
-        return self.transform_screen_data(screen)
-
-    def crop_screen(self, screen):
-        screen_height = screen.shape[1]
-
-        top = int(screen_height * 0.4)
-        bottom = int(screen_height * 0.8)
-        screen = screen[:, top:bottom, :]
-        return screen
-
-    def transform_screen_data(self, screen):
-        screen = np.ascontiguousarray(screen, dtype=np.float32) / 255
-        screen = torch.from_numpy(screen)
-
-        resize = T.Compose([
-            T.ToPILImage(), T.Resize((40, 90)), T.ToTensor()])
-        return resize(screen).unsqueeze(0).to(self.device)
+    
 
 
-device = torch.device('cuda' if torch.cuda.is_available() else 'cpu')
-env = Env(device)
-env.reset()
-
-
-def get_current(policy_net, states, actions):
-    return policy_net(states).gather(dim=1, index=actions.unsqueeze(-1))
-
-def get_next(target_net, next_states):                
-    final_state_locations = next_states.flatten(start_dim=1) \
-        .max(dim=1)[0].eq(0).type(torch.bool)
-    non_final_state_locations = (final_state_locations == False)
-    non_final_states = next_states[non_final_state_locations]
-    batch_size = next_states.shape[0]
-    values = torch.zeros(batch_size).to(device)
-    values[non_final_state_locations] = target_net(non_final_states).max(dim=1)[0].detach()
-    return values
-
-
-def extract_tensors(experiences):
-    # Convert batch of Experiences to Experience of batches
-    batch = Experience(*zip(*experiences))
-
-    t1 = torch.cat(batch.state)
-    t2 = torch.cat(batch.action)
-    t3 = torch.cat(batch.reward)
-    t4 = torch.cat(batch.next_state)
-
-    return (t1, t2, t3, t4)
-
-def get_moving_average(period, values):     #Calculate average of last 100 episodes
+def get_moving_average(period, values):     #Calculate the average of last 100 episodes
     values = torch.tensor(values, dtype=torch.float)
     if len(values) >= period:
         moving_avg = values.unfold(dimension=0, size=period, step=1).mean(dim=1).flatten(start_dim=0)
@@ -188,7 +129,8 @@ def get_moving_average(period, values):     #Calculate average of last 100 episo
         moving_avg = torch.zeros(len(values))
         return moving_avg.numpy()
 
-def plot(values, moving_avg_period):
+
+def plot(values, moving_avg_period):    #Helper function to plot the durations vs episodes
     plt.figure(2)
     plt.clf()
     plt.title('Training...')
@@ -198,48 +140,82 @@ def plot(values, moving_avg_period):
 
     moving_avg = get_moving_average(moving_avg_period, values)
     plt.plot(moving_avg)
+    print("Episode", len(values), "\n", moving_avg_period, "episode moving avg:", moving_avg[-1])
     plt.savefig("cartpole.png")
     plt.pause(0.001)
     
 
+def train(states, actions, rewards, next_states, dones):            #Train the network
+    max_q_values = target_net(next_states).max(-1).values
+    target_q_values = (1. - dones) * gamma * max_q_values + rewards
+    
+    current_q_values = policy_net(states)
+    action_masks = F.one_hot(actions, num_actions)
+    masked_q_values = torch.sum(action_masks * current_q_values, dim = -1)
+    loss = F.mse_loss(masked_q_values, target_q_values.detach())
+    optimizer.zero_grad()
+    loss.backward()
+    optimizer.step()
+    return loss
 
-agent = Agent(env.num_actions_available(), epsilon, device)
-memory = ReplayMemory(memory_size)
-policy_net = DQN(env.get_screen_height(), env.get_screen_width()).to(device)
-target_net = DQN(env.get_screen_height(), env.get_screen_width()).to(device)
+def main():
+    agent = Agent(num_actions, eps_start, eps_end, eps_decay)
+    memory = ReplayMemory(memory_size)
+    
+    target_net.eval()
+    episode_durations = []
+    
+    my_lr_scheduler = optim.lr_scheduler.ExponentialLR(optimizer=optimizer, gamma=0.9)     #Learning rate scheduler
+    solved = False
+
+    for episode in range(num_episodes):
+        state = env.reset().astype(np.float32)
+        for timestep in count():
+            state_in = torch.from_numpy(np.expand_dims(state, axis=0)).to(device)
+            action = agent.select_action(state_in)
+            next_state, reward, done, _ = env.step(action)
+            next_state = next_state.astype(np.float32)
+            memory.push(Experience(state, action, next_state, reward, done))
+            state = next_state
+            if memory.can_provide_sample(batch_size):                                       #sample a random batch from buffer to train the network
+                states, actions, rewards, next_states, dones = memory.sample(
+                    batch_size)
+                loss = train(states, actions, rewards, next_states, dones)
+            if done:
+                episode_durations.append(timestep)
+                plot(episode_durations, 100)
+                break
+        if episode % 5 == 0:
+            my_lr_scheduler.step()
+        if episode % target_update == 0:
+            target_net.load_state_dict(policy_net.state_dict())                             #Update the parameters of target network from policy network
+    env.close()
+
+#Function to play
+def play(): 
+    score = 0
+    policy_net = torch.load("blob/cartpole_best.pth")
+    for episode in range(100):
+        state = env.reset().astype(np.float32)
+        done = False
+        score = 0
+        while not done:
+            state_in = torch.from_numpy(np.expand_dims(state, axis = 0)).float().to(device)
+            action = policy_net(state_in).cpu().data.numpy()
+            action = np.argmax(action)
+            next_state, reward, done, _ = env.step(action)
+            score += reward
+            env.render()
+            state = next_state
+        print(f"Score for this round: {score}")
+
 env.close()
-target_net.load_state_dict(policy_net.state_dict())
-target_net.eval()
 
-optimizer = optim.Adam(params=policy_net.parameters(), lr=lr)
-episode_durations = []
-
-for episode in range(num_episodes):
-    env.reset()
-    state = env.get_state()
-    for timestep in count():
-        action = agent.select_action(state, policy_net)
-        reward = env.take_action(action)
-        next_state = env.get_state()
-        memory.push(Experience(state, action, next_state, reward))
-        state = next_state
-        if memory.can_provide_sample(batch_size):
-            experiences = memory.sample(batch_size)
-            states, actions, rewards, next_states = extract_tensors(
-                experiences)
-            current_q_values = get_current(policy_net, states, actions)
-            next_q_values = get_next(target_net, next_states)
-            target_q_values = (next_q_values * gamma) + rewards
-
-            loss = F.mse_loss(current_q_values, target_q_values.unsqueeze(1))
-            optimizer.zero_grad()
-            loss.backward()
-            optimizer.step()
-        if env.done:
-            episode_durations.append(timestep)
-            plot(episode_durations, 100)
-            break
-    if episode % target_update == 0:
-        target_net.load_state_dict(policy_net.state_dict())
-
-env.close()
+if __name__ == "__main__":
+    if 'play' in sys.argv:
+        play()
+    elif 'train' in sys.argv:
+        main()
+    else:
+        print("Please specify your choice")
+    
